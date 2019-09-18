7e18c23e94ac9a77edeb1176efba75fd4d1499a9
Moosan <washer2016@outlook.jp>
2019-09-18 21:01:37 +0900

    Velocity

--- Assets/Moosan/Scripts/Test/Throwable.cs
+++ Assets/Moosan/Scripts/Test/Throwable.cs
@@ -4,13 +4,14 @@ namespace Assets.Moosan.Scripts.Test
 {
     public class Throwable : OVRGrabbable
     {
-        //引数の速度を、質量1の時に与えられた速度と仮定し、力に戻している
+        //v = Ft/m
+        //v_c = Ft/1 = Ft
+        //v = v_c/m
         public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
         { 
             Rigidbody rb = gameObject.GetComponent<Rigidbody>();
-            var F = linearVelocity / Time.deltaTime;
             rb.isKinematic = m_grabbedKinematic;
-            rb.AddForce(F, ForceMode.Force);
+            rb.velocity = linearVelocity / rb.mass;
             rb.angularVelocity = angularVelocity;
             m_grabbedBy = null;
             m_grabbedCollider = null;
