ec3d14ef4453a44fa1a24e2a2f913103524eee4b
HnakadouriN <hasebenaoki0908@docomo.ne.jp>
2019-09-21 06:33:16 +0900

    結構完成

---
+++ Assets/ShrowStars/_Scripts/SyurikenSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyurikenSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject Obj;
+    private GameObject pos;
+    private bool isSpawning = false;
+    private OVRGrabbable grabbable;
+
+    private void Start() {
+        Spawn();
+    }
+    private void Update() {
+        if(grabbable.isGrabbed){
+            if(isSpawning)return;
+            grabbable = null;
+            isSpawning = true;
+            Invoke("Spawn",1.0f);
+        }else
+        {
+            if(isSpawning||grabbable == null)return;
+            grabbable.transform.position = transform.position;
+            grabbable.transform.rotation = transform.rotation;
+        }
+    }
+    private void Spawn(){
+        pos = Instantiate(Obj,this.transform.position,this.transform.rotation);
+        grabbable = pos.GetComponent<OVRGrabbable>();
+        isSpawning = false;
+    }
+}
--- Assets/ShrowStars/_Scripts/Throwable.cs
+++ Assets/ShrowStars/_Scripts/Throwable.cs
@@ -5,13 +5,19 @@ namespace Assets.Moosan.Scripts.Test
     public class Throwable : OVRGrabbable
     {
         [SerializeField] private Needle _needle;
+        [SerializeField] private Collider[] Colliders;
         //v = Ft/m
         //v_c = Ft/1 = Ft
         //v = v_c/m
         public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
-        { 
+        {
             Rigidbody rb = gameObject.GetComponent<Rigidbody>();
-            rb.isKinematic = m_grabbedKinematic;
+            foreach (var collider in Colliders)
+            {
+                collider.isTrigger = false;
+            }
+            rb.isKinematic = false;
+            //rb.isKinematic = m_grabbedKinematic;
             rb.velocity = linearVelocity / rb.mass;
             rb.angularVelocity = angularVelocity  * -5f;
             m_grabbedBy = null;
