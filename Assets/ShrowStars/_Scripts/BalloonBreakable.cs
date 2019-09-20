c8be4385399557b203be03fa4dd327db0f6eeac2
Moosan <washer2016@outlook.jp>
2019-09-21 02:23:18 +0900

    AddBalloon

---
+++ Assets/ShrowStars/_Scripts/BalloonBreakable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class BalloonBreakable : MonoBehaviour, IBreakable
+{
+    [SerializeField]
+    private BalloonColor Color;
+    [SerializeField]
+    private GameObject HaretuPrefab;
+    public void Break()
+    {
+        if (HaretuPrefab != null)
+        {
+            Instantiate(HaretuPrefab, transform.position, Quaternion.identity);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/ShrowStars/_Scripts/BalloonColor.cs
@@ -0,0 +1,4 @@
+﻿public enum BalloonColor
+{
+    red,green,blue
+}
