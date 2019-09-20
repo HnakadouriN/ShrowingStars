61f9686d77071077c946e23975341233f6bad0c2
HnakadouriN <hasebenaoki0908@docomo.ne.jp>
2019-09-20 17:06:32 +0900

    風船実装

---
+++ Assets/ShrowStars/_Scripts/BalloonController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ShrowStars._Scripts
+{
+    public class BalloonController: MonoBehaviour
+    {
+        [SerializeField] private GameObject _B_object;
+
+        private Vector3 AppendPosi;
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartCoroutine(Waves());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        private IEnumerator Waves()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                SpawnWaves();
+                yield return new WaitForSeconds(1f);
+            }
+            yield return null;
+        }
+
+        private void SpawnWaves()
+        {
+        
+            AppendPosi = new Vector3(Random.Range(-10,10),Random.Range(0,20),Random.Range(-10,10));
+            Instantiate(_B_object, AppendPosi, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/ShrowStars/_Scripts/BalloonMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace ShrowStars._Scripts
+{
+    public class BalloonMove : MonoBehaviour
+    {
+        private Rigidbody rb;
+
+        private float DestroyTime = 10f;
+
+        private float MeasurementTime;
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            rb.AddForce(Vector3.up * 8f,ForceMode.Force);
+            MeasurementTime += Time.deltaTime;
+            if (MeasurementTime > DestroyTime)
+            {
+                Destroy(this.gameObject);
+                MeasurementTime = 0;
+            }
+        }
+    }
+}
