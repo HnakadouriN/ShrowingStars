edd466956aafb27ce6d2d066cc2be60308d9c918
Moosan <washer2016@outlook.jp>
2019-09-21 06:25:11 +0900

    AddUIPrefab

---
+++ Assets/ShrowStars/_Scripts/CountUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountUI : MonoBehaviour
+{
+    private TextMesh text;
+    private void Awake()
+    {
+        text = GetComponent<TextMesh>();
+        SetCount(0);
+    }
+    void SetCount(int count)
+    {
+        text.text = "現在の個数: " + count + "個";
+    }
+}
